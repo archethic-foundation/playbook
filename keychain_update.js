4f6ffb49e9d786dad4f1b3d59ddf667831dbed84
bchamagne <74045243+bchamagne@users.noreply.github.com>
2023-10-27 16:49:15 +0200

    Use Winston to log, refactor all run functions to return a promise (#2)

--- keychain_update.js
+++ keychain_update.js
@@ -1,19 +1,21 @@
 import Archethic, { Keychain, Crypto, Utils } from "@archethicjs/sdk"
 import { randomBytes } from "crypto"
+import { getLogger } from "./logger.js"
 
+const logger = getLogger()
 const originPrivateKey = Utils.originPrivateKey;
 let archethic
 
 function generateKeychainTransaction(accessPublicKey, keychain = undefined, index = 0) {
     const keychainSeed = randomBytes(32)
 
-    if(keychain === undefined) {
+    if (keychain === undefined) {
         keychain = new Keychain(keychainSeed)
             .addService("uco", "m/650'/0/0")
             .addAuthorizedPublicKey(accessPublicKey)
     }
 
-   return archethic.account.newKeychainTransaction(keychain, index)
+    return archethic.account.newKeychainTransaction(keychain, index)
         .originSign(originPrivateKey)
 }
 
@@ -25,89 +27,104 @@ function generateKeychainAccessTransaction(accessSeed, keychainAddress) {
 
 async function run() {
 
-    const endpoint = process.env["ENDPOINT"] || "https://testnet.archethic.net"
-    
-    archethic = new Archethic(endpoint)
-    await archethic.connect()
+    return new Promise(async function (resolve, reject) {
+        try {
+            const endpoint = process.env["ENDPOINT"] || "https://testnet.archethic.net"
 
-    const accessSeed = randomBytes(32)
-    const { publicKey: accessPublicKey } = Crypto.deriveKeyPair(accessSeed, 0);
+            archethic = new Archethic(endpoint)
+            await archethic.connect()
 
-    const keychainTx = generateKeychainTransaction(accessPublicKey)
+            const accessSeed = randomBytes(32)
+            const { publicKey: accessPublicKey } = Crypto.deriveKeyPair(accessSeed, 0);
 
-    keychainTx
-        .on("sent", () => {
-            console.log("Keychain transaction sent")
-            console.log("Await validation ...")
-        })
-        .on("requiredConfirmation", async (_confirmations, sender) => {
-            sender.unsubscribe()
+            const keychainTx = generateKeychainTransaction(accessPublicKey)
 
-            console.log(`Keychain transaction created - ${Utils.uint8ArrayToHex(keychainTx.address)}`)
-
-            const accessKeychainTx = generateKeychainAccessTransaction(accessSeed, keychainTx.address)
-
-            accessKeychainTx
+            keychainTx
                 .on("sent", () => {
-                    console.log("Keychain access transaction sent")
-                    console.log("Await validation ...")
+                    logger.debug("Keychain transaction sent")
+                    logger.debug("Await validation ...")
                 })
-                .on("requiredConfirmation", async (confirmation, sender) => {
+                .on("requiredConfirmation", async (_confirmations, sender) => {
                     sender.unsubscribe()
 
-                    console.log(`Keychain access transaction created - ${Utils.uint8ArrayToHex(accessKeychainTx.address)}`)
+                    logger.debug(`Keychain transaction created - ${Utils.uint8ArrayToHex(keychainTx.address)}`)
 
-                    console.log("Keychain fetching...")
-                    const keychain = await archethic.account.getKeychain(accessSeed)
+                    const accessKeychainTx = generateKeychainAccessTransaction(accessSeed, keychainTx.address)
 
-                    if (new TextDecoder().decode(keychainTx.data.content) != JSON.stringify(keychain.toDID())) {
-                        console.log("Keychain doesn't match")
-                        process.exit(1)
-                    }
-
-                    console.log("Keychain retrieved with success")
-
-                    keychain.addService("website", "m/650'/website/0")
-                    const newKeychainTx = generateKeychainTransaction(accessPublicKey, keychain, 1)
-
-                    newKeychainTx
+                    accessKeychainTx
                         .on("sent", () => {
-                            console.log("Keychain's update transaction sent")
-                            console.log("Await validation ...")
+                            logger.debug("Keychain access transaction sent")
+                            logger.debug("Await validation ...")
                         })
                         .on("requiredConfirmation", async (confirmation, sender) => {
                             sender.unsubscribe()
-                            console.log(`Keychain's update transaction created - ${Utils.uint8ArrayToHex(newKeychainTx.address)}`)
 
-                            console.log("Keychain fetching...")
+                            logger.debug(`Keychain access transaction created - ${Utils.uint8ArrayToHex(accessKeychainTx.address)}`)
+
+                            logger.debug("Keychain fetching...")
                             const keychain = await archethic.account.getKeychain(accessSeed)
 
-                            if (keychain.services.website === undefined) {
-                                console.log(`Keychain' update doestn't match`)
-                                process.exit(1)
+                            if (new TextDecoder().decode(keychainTx.data.content) != JSON.stringify(keychain.toDID())) {
+                                reject("Keychain doesn't match")
+                                return
                             }
 
-                            console.log("Keychain updated with success")
+                            logger.debug("Keychain retrieved with success")
 
-                            process.exit(0);
+                            keychain.addService("website", "m/650'/website/0")
+                            const newKeychainTx = generateKeychainTransaction(accessPublicKey, keychain, 1)
+
+                            newKeychainTx
+                                .on("sent", () => {
+                                    logger.debug("Keychain's update transaction sent")
+                                    logger.debug("Await validation ...")
+                                })
+                                .on("requiredConfirmation", async (confirmation, sender) => {
+                                    sender.unsubscribe()
+                                    logger.debug(`Keychain's update transaction created - ${Utils.uint8ArrayToHex(newKeychainTx.address)}`)
+
+                                    logger.debug("Keychain fetching...")
+                                    const keychain = await archethic.account.getKeychain(accessSeed)
+
+                                    if (keychain.services.website === undefined) {
+                                        reject(`Keychain' update doestn't match`)
+                                        return
+                                    }
+
+                                    resolve("Keychain updated with success")
+                                    return
+                                })
+                                .on("error", (context, reason) => {
+                                    reject(`Keychain's update transaction failed - ${reason}`)
+                                    return
+                                })
+                                .send()
                         })
                         .on("error", (context, reason) => {
-                            console.log(`Keychain's update transaction failed - ${reason}`)
-                            process.exit(1)
+                            reject(`Keychain accesss transaction failed - ${reason}`)
+                            return
                         })
                         .send()
                 })
                 .on("error", (context, reason) => {
-                    console.log(`Keychain accesss transaction failed - ${reason}`)
-                    process.exit(1)
+                    reject(`Keychain transaction failed - ${reason}`)
+                    return
                 })
                 .send()
-        })
-        .on("error", (context, reason) => {
-            console.log(`Keychain transaction failed - ${reason}`)
-            process.exit(1)
-        })
-        .send()
+        } catch (e) {
+            reject(e)
+            return
+        }
+    })
 }
 
 run()
+    .then((msg) => {
+        logger.info(msg)
+        return 0
+    })
+    .catch((msg) => {
+        logger.error(msg)
+        return 1
+    })
+    .then(logger.exit_when_flush)
